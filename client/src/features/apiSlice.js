dbc4cd8bbfb05a315c9138b1d956a126db424427
ARPIT GUPTA <arpitgupta205001@gmail.com>
2025-05-02 12:25:52 +0530

    Update apiSlice.js

--- client/src/features/apiSlice.js
+++ client/src/features/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const progressApi = createApi({
   reducerPath: 'progressApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://video-progress-tracker-backend-0nz6.onrender.com' }),
   endpoints: (builder) => ({
     getProgress: builder.query({
       query: ({ userId, videoId }) => `progress/${userId}/${videoId}`,
